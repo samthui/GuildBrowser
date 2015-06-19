ded761556f500f4190f38dc9b594ab2807935b27
Sam <samthui7@gmail.com>
2015-06-19 16:17:06 +0700

    added test target

---
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.m
@@ -0,0 +1,34 @@
+//
+//  GuildBrowserLogicTests.m
+//  GuildBrowserLogicTests
+//
+//  Created by Phien Tram on 6/19/15.
+//  Copyright (c) 2015 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GuildBrowserLogicTests : SenTestCase
+
+@end
+
+@implementation GuildBrowserLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+//- (void)testExample
+//{
+//    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+//}
+
+@end
